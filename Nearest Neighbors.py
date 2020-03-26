4bc771534371ace8e9ac621d130489024cce5a9a
anandhamurthy <anandhamurthy2000@gmail.com>
2020-03-26 19:43:16 +0530

    first

---
+++ Nearest Neighbors.py
@@ -0,0 +1,8 @@
+from sklearn.neighbors import NearestCentroid
+import numpy as np
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+y = np.array([1, 1, 1, 2, 2, 2])
+clf = NearestCentroid()
+clf.fit(X, y)
+
+print(clf.predict([[-0.8, -1]]))
